c456da16e25e9571d1350367966a09b1fd70e5e2
William Chang <william20021026@gmail.com>
2017-07-27 13:08:41 +0800

    update footer file

--- html/footer.php
+++ html/footer.php
@@ -2,4 +2,4 @@
 <li class="divider"></li>
 </ul>
 </br><i class="fa fa-chevron-left" aria-hidden="true"></i> <i class="fa fa-chevron-right" aria-hidden="true"></i> with <i class="fa fa-heart" aria-hidden="true"></i> by <b>William Chang</b><br>
-<small class="text-center">© 2017 William Chang.</small>
+<small class="text-center">© 2017 William Chang. Open Source on <a href="https://github.com/wilicw/wurl.ga"><i class="fa fa-github" aria-hidden="true"></i>Github</a></small>
