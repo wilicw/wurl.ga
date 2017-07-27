fe4e997291da58c81c6cad60d1c45994d09e63d0
William Chang <william20021026@gmail.com>
2017-07-27 19:01:52 +0800

    Add files via upload

--- html/footer.php
+++ html/footer.php
@@ -1,5 +1,3 @@
-<ul class="nav nav-list">
-<li class="divider"></li>
-</ul>
+<ul class="nav nav-list"><li class="divider"></li></ul>
 </br><i class="fa fa-chevron-left" aria-hidden="true"></i> <i class="fa fa-chevron-right" aria-hidden="true"></i> with <i class="fa fa-heart" aria-hidden="true"></i> by <b>William Chang</b><br>
-<small class="text-center">© 2017 William Chang. Open Source on <a href="https://github.com/wilicw/wurl.ga"><i class="fa fa-github" aria-hidden="true"></i>Github</a></small>
+<small class="text-center">© 2017 William Chang. Open Source on <a href="https://github.com/wilicw/wurl.ga"> <i class="fa fa-github" aria-hidden="true"></i>Github</a></small>
